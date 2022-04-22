4e0d774ac82b3f276b632472f1cd8c07c390559e
안유진 <Eugenius1st@gmail.com>
2022-04-22 22:30:28 +0900

    [#3]: Basic Animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
-// npm install framer-motion
-
 const Wrapper = styled.div`
     height: 100vh;
     width: 100vw;
@@ -11,20 +9,20 @@ const Wrapper = styled.div`
     align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
+    //motion div에 적용할 수 있다
     width: 200px;
     height: 200px;
     background-color: white;
-    border-radius: 10px;
+    border-radius: 15px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 export default function App() {
     return (
         <Wrapper>
-            <Box />
-            {/* 뭐든 animate 하길 원한다면 motion패키지로부터 나와야 한다 */}
-            <motion.div></motion.div>
+            {/* <Box transition={{ duration: 3 }} animate={{ borderRadius: "100px" }} /> */}
+            <Box transition={{ type: "spring", delay: 1 }} initial={{ scale: 0 }} animate={{ scale: 1, rotateZ: 360 }} />
         </Wrapper>
     );
 }
