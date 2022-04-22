68d2a937ac0e097ea023ff196eaf3793ccfe1daa
안유진 <Eugenius1st@gmail.com>
2022-04-22 22:43:23 +0900

    [#4]: Variants part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
-
+// Variants_애니메이션의 무대이다. 한 장소에 설정을 넣어주기만 하면 되고, motion들은 알아서 찾아낸다.
 const Wrapper = styled.div`
     height: 100vh;
     width: 100vw;
@@ -10,7 +10,6 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
-    //motion div에 적용할 수 있다
     width: 200px;
     height: 200px;
     background-color: white;
@@ -18,11 +17,16 @@ const Box = styled(motion.div)`
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars = {
+    start: { scale: 0 },
+    end: { scale: 1, rotateX: 360, transition: { type: "spring", delay: 1 } },
+};
+
 export default function App() {
     return (
         <Wrapper>
-            {/* <Box transition={{ duration: 3 }} animate={{ borderRadius: "100px" }} /> */}
-            <Box transition={{ type: "spring", delay: 1 }} initial={{ scale: 0 }} animate={{ scale: 1, rotateZ: 360 }} />
+            <Box variants={myVars} initial="start" animate="end" />
+            {/* myVars를 들여다 본 후 start와 end를 찾아준다. */}
         </Wrapper>
     );
 }
