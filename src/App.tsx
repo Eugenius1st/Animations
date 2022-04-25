7df3c250148eecfafdc96faa72160f12c14466a6
안유진 <Eugenius1st@gmail.com>
2022-04-25 17:49:59 +0900

    [#16]: Slider part One

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ const Wrapper = styled(motion.div)`
     justify-content: center;
     align-items: center;
     background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 253));
+    flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
@@ -18,6 +19,10 @@ const Box = styled(motion.div)`
     background-color: rgba(255, 255, 255, 1);
     border-radius: 40px;
     position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 28px;
     top: 100px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
@@ -39,14 +44,30 @@ const boxVariants = {
     },
 };
 
+const box = {
+    invisible: { x: 500, opacity: 0, scale: 0 },
+    visible: { x: 0, opacity: 1, scale: 1, transition: { duration: 1 } },
+    exit: { x: -500, opacity: 0, scale: 0, rotateX: 180, transition: { duration: 1 } },
+};
+
 export default function App() {
-    const [showing, setShowing] = useState(false);
-    const toggleShowing = () => setShowing((prev) => !prev);
+    const [visible, setVisible] = useState(1);
+    const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+    const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
     return (
         <Wrapper>
-            <button onClick={toggleShowing}>Click</button>
-            <AnimatePresence>{showing ? <Box variants={boxVariants} initial="initial" animate="visible" exit="leaving" /> : null}</AnimatePresence>
-            {/* AnimatePresence 는 Box가 사라지는 것을 animate 한다 */}
+            <AnimatePresence>
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+                    i === visible ? (
+                        <Box variants={box} initial="invisible" animate="visible" exit="exit" key={i}>
+                            {i}
+                        </Box>
+                    ) : null
+                )}
+            </AnimatePresence>
+            {/* visible index에 따라 하나만 보인다 */}
+            <button onClick={nextPlease}>next</button>
+            <button onClick={prevPlease}>prev</button>
         </Wrapper>
     );
 }
