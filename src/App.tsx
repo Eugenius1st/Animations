5afa8fb79f357df3ea24f55dfde323cba78433c6
안유진 <Eugenius1st@gmail.com>
2022-04-26 22:23:29 +0900

    [#19]: Slider part Two

--- src/App.tsx
+++ src/App.tsx
@@ -27,43 +27,34 @@ const Box = styled(motion.div)`
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-    initial: {
-        opacity: 0,
-        scale: 0,
-    },
-    visible: {
-        opacity: 1,
-        scale: 1,
-        rotateZ: 360,
-    },
-    leaving: {
-        opacity: 0,
-        scale: 0,
-        y: 50,
-    },
-};
-
 const box = {
-    invisible: { x: 500, opacity: 0, scale: 0 },
-    visible: { x: 0, opacity: 1, scale: 1, transition: { duration: 1 } },
-    exit: { x: -500, opacity: 0, scale: 0, rotateX: 180, transition: { duration: 1 } },
+    entry: (isBack: boolean) => ({
+        x: isBack ? -500 : 500,
+        opacity: 0,
+        scale: 0,
+    }),
+    center: { x: 0, opacity: 1, scale: 1, transition: { duration: 1 } },
+    exit: (isBack: boolean) => ({
+        x: isBack ? 500 : -500,
+        opacity: 0,
+        scale: 0,
+        transition: { duration: 0.2 },
+    }),
 };
 
 export default function App() {
     const [visible, setVisible] = useState(1);
-    const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-    const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+    const [back, setBack] = useState(false);
+    const nextPlease = () => setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+    const prevPlease = () => setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
     return (
         <Wrapper>
-            <AnimatePresence>
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
-                    i === visible ? (
-                        <Box variants={box} initial="invisible" animate="visible" exit="exit" key={i}>
-                            {i}
-                        </Box>
-                    ) : null
-                )}
+            <AnimatePresence exitBeforeEnter custom={back}>
+                <Box custom={back} variants={box} initial="entry" animate="center" exit="exit" key={visible}>
+                    {visible}
+                </Box>
             </AnimatePresence>
             {/* visible index에 따라 하나만 보인다 */}
             <button onClick={nextPlease}>next</button>
