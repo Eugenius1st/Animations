c71e23e6383dfc3900976e46e99c7f8527396e94
안유진 <Eugenius1st@gmail.com>
2022-04-30 23:15:13 +0900

    [#22]: Final Project Part one

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
+
+//Box 들을 클릭하면 중앙 정렬되도록 하고 animate도 적용되도록 한다.
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
@@ -11,42 +13,62 @@ const Wrapper = styled(motion.div)`
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 253));
 `;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 80vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  width: 100%;
+  height: 200px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  height: 100px;
-  width: 100px;
-  border-radius: 50%;
+const Overlay = styled(motion.div)`
+  // 오버레이 되었을때 배경을 흐리게 해주는 효과
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default function App() {
+  //클릭된 요소를 position absoulte 된 element 상태로 만들어줘야 한다.
   const [clicked, setClicked] = useState(false);
-  const toggleClicked = () => setClicked((prev) => !prev);
+  const toggle = () => setClicked((prev) => !prev);
   return (
-    <Wrapper onClick={toggleClicked}>
-      <Box>
-        {!clicked ? (
-          <Circle layoutId="circle" style={{ borderRadius: 50 }} />
-        ) : null}
-      </Box>
-      {/* layout을 이용해 부드럽게 element를 움직일 수 있도록 한다 */}
-      <Box>
+    <Wrapper onClick={toggle}>
+      <Grid>
+        <Box layoutId="hello" />
+        <Box />
+        <Box />
+        <Box />
+      </Grid>
+      <AnimatePresence>
         {clicked ? (
-          <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
+          <Overlay
+            initial={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+            animate={{ backgroundColor: 'rgba(0, 0, 0, 0.8)' }}
+            exit={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+          >
+            <Box layoutId="hello" style={{ width: 500, height: 250 }} />
+            {/* 같은 layout아이디를 가지고 있으므로 애니매이션이 달라진다
+            Box 의 가장 위에서 Overlay의 중앙으로 갈 것이다.  
+            이 모든 것은 layoutId 덕분에 멋진 motion을 가질 수 있게 된 것이다.*/}
+          </Overlay>
         ) : null}
-      </Box>
-      {/* Framer는 우리를 위한 모든 animation을 만들어준다, 그냥 Circle과 새로운 Circle이 어디있는지만 알려주면 된다 */}
-      {/* Framer은 둘을 이어주기만 한다 */}
+      </AnimatePresence>
+      {/* animate될 div 이므로 (motion.div)로 Overlay를 바꿔주어야 한다. */}
     </Wrapper>
   );
 }
